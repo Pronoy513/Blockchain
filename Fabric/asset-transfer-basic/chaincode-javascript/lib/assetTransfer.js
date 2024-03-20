6cfc47d263bc29221b820bceb4512f100807f66b
selvaragulkumar <selvaragulkumar@gmail.com>
2024-03-20 23:42:35 +0530

    Modified Chaincode Js in AssetTransfer_basic

--- Fabric/asset-transfer-basic/chaincode-javascript/lib/assetTransfer.js
+++ Fabric/asset-transfer-basic/chaincode-javascript/lib/assetTransfer.js
@@ -1,151 +1,150 @@
-/*
- * Copyright IBM Corp. All Rights Reserved.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 'use strict';
 
-// Deterministic JSON.stringify()
-const stringify  = require('json-stringify-deterministic');
-const sortKeysRecursive  = require('sort-keys-recursive');
+const stringify = require('json-stringify-deterministic');
+const sortKeysRecursive = require('sort-keys-recursive');
 const { Contract } = require('fabric-contract-api');
 
-class AssetTransfer extends Contract {
+class ArtifactTransfer extends Contract {
 
     async InitLedger(ctx) {
-        const assets = [
-            {
-                ID: 'asset1',
-                Color: 'blue',
-                Size: 5,
-                Owner: 'Tomoko',
-                AppraisedValue: 300,
-            },
-            {
-                ID: 'asset2',
-                Color: 'red',
-                Size: 5,
-                Owner: 'Brad',
-                AppraisedValue: 400,
-            },
-            {
-                ID: 'asset3',
-                Color: 'green',
-                Size: 10,
-                Owner: 'Jin Soo',
-                AppraisedValue: 500,
-            },
-            {
-                ID: 'asset4',
-                Color: 'yellow',
-                Size: 10,
-                Owner: 'Max',
-                AppraisedValue: 600,
-            },
-            {
-                ID: 'asset5',
-                Color: 'black',
-                Size: 15,
-                Owner: 'Adriana',
-                AppraisedValue: 700,
-            },
-            {
-                ID: 'asset6',
-                Color: 'white',
-                Size: 15,
-                Owner: 'Michel',
-                AppraisedValue: 800,
-            },
-        ];
-
-        for (const asset of assets) {
-            asset.docType = 'asset';
-            // example of how to write to world state deterministically
-            // use convetion of alphabetic order
-            // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-            // when retrieving data, in any lang, the order of data will be the same and consequently also the corresonding hash
-            await ctx.stub.putState(asset.ID, Buffer.from(stringify(sortKeysRecursive(asset))));
-        }
+        // Initialize ledger with sample data
     }
 
-    // CreateAsset issues a new asset to the world state with given details.
-    async CreateAsset(ctx, id, color, size, owner, appraisedValue) {
-        const exists = await this.AssetExists(ctx, id);
+    async CreateArtifact(ctx, id, issuer, createdDate, expiryDate, checksum) {
+        const exists = await this.ArtifactExists(ctx, id);
         if (exists) {
-            throw new Error(`The asset ${id} already exists`);
+            throw new Error(`The artifact ${id} already exists`);
         }
 
-        const asset = {
+        // Create whole artifact
+        const wholeArtifact = {
             ID: id,
-            Color: color,
-            Size: size,
-            Owner: owner,
-            AppraisedValue: appraisedValue,
+            Issuer: issuer,
+            CreatedDate: createdDate,
+            ExpiryDate: expiryDate,
+            Checksum: checksum,
+            Label: 'whole',
         };
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
-        return JSON.stringify(asset);
-    }
+        await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(wholeArtifact))));
 
-    // ReadAsset returns the asset stored in the world state with given id.
-    async ReadAsset(ctx, id) {
-        const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
-        if (!assetJSON || assetJSON.length === 0) {
-            throw new Error(`The asset ${id} does not exist`);
+        // Create segment artifacts
+        const segmentArtifacts = [];
+        const data = JSON.stringify(wholeArtifact);
+        const segmentSize = Math.ceil(data.length / 3);
+        for (let i = 0; i < 3; i++) {
+            const segmentData = data.substring(i * segmentSize, (i + 1) * segmentSize);
+            const segmentArtifact = {
+                ID: `${id}_segment${i}`,
+                DataSegment: segmentData,
+                Label: 'segment',
+            };
+            await ctx.stub.putState(`${id}_segment${i}`, Buffer.from(stringify(sortKeysRecursive(segmentArtifact))));
+            segmentArtifacts.push(segmentArtifact);
         }
-        return assetJSON.toString();
+
+        // Create checksum artifact
+        const checksumArtifact = {
+            ID: `${id}_checksum`,
+            Checksum: checksum,
+            Label: 'checksum',
+        };
+        await ctx.stub.putState(`${id}_checksum`, Buffer.from(stringify(sortKeysRecursive(checksumArtifact))));
+
+        return {
+            wholeArtifact,
+            segmentArtifacts,
+            checksumArtifact
+        };
     }
 
-    // UpdateAsset updates an existing asset in the world state with provided parameters.
-    async UpdateAsset(ctx, id, color, size, owner, appraisedValue) {
-        const exists = await this.AssetExists(ctx, id);
+    async ReadArtifact(ctx, id) {
+        const artifactJSON = await ctx.stub.getState(id);
+        if (!artifactJSON || artifactJSON.length === 0) {
+            throw new Error(`The artifact ${id} does not exist`);
+        }
+        return artifactJSON.toString();
+    }
+
+    async UpdateArtifact(ctx, id, issuer, createdDate, expiryDate, checksum) {
+        const exists = await this.ArtifactExists(ctx, id);
         if (!exists) {
-            throw new Error(`The asset ${id} does not exist`);
+            throw new Error(`The artifact ${id} does not exist`);
         }
 
-        // overwriting original asset with new asset
-        const updatedAsset = {
+        const updatedArtifact = {
             ID: id,
-            Color: color,
-            Size: size,
-            Owner: owner,
-            AppraisedValue: appraisedValue,
+            Issuer: issuer,
+            CreatedDate: createdDate,
+            ExpiryDate: expiryDate,
+            Checksum: checksum,
+            Label: 'whole',
         };
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        return ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(updatedAsset))));
-    }
+        await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(updatedArtifact))));
 
-    // DeleteAsset deletes an given asset from the world state.
-    async DeleteAsset(ctx, id) {
-        const exists = await this.AssetExists(ctx, id);
-        if (!exists) {
-            throw new Error(`The asset ${id} does not exist`);
+        // Update segment artifacts
+        const data = JSON.stringify(updatedArtifact);
+        const segmentSize = Math.ceil(data.length / 3);
+        for (let i = 0; i < 3; i++) {
+            const segmentData = data.substring(i * segmentSize, (i + 1) * segmentSize);
+            const segmentArtifact = {
+                ID: `${id}_segment${i}`,
+                DataSegment: segmentData,
+                Label: 'segment',
+            };
+            await ctx.stub.putState(`${id}_segment${i}`, Buffer.from(stringify(sortKeysRecursive(segmentArtifact))));
         }
-        return ctx.stub.deleteState(id);
+
+        // Update checksum artifact
+        const checksumArtifact = {
+            ID: `${id}_checksum`,
+            Checksum: checksum,
+            Label: 'checksum',
+        };
+        await ctx.stub.putState(`${id}_checksum`, Buffer.from(stringify(sortKeysRecursive(checksumArtifact))));
+
+        return {
+            wholeArtifact: updatedArtifact,
+            segmentArtifacts: [],
+            checksumArtifact
+        };
     }
 
-    // AssetExists returns true when asset with given ID exists in world state.
-    async AssetExists(ctx, id) {
-        const assetJSON = await ctx.stub.getState(id);
-        return assetJSON && assetJSON.length > 0;
+    async DeleteArtifact(ctx, id) {
+        const exists = await this.ArtifactExists(ctx, id);
+        if (!exists) {
+            throw new Error(`The artifact ${id} does not exist`);
+        }
+
+        // Delete whole artifact
+        await ctx.stub.deleteState(id);
+
+        // Delete segment artifacts
+        for (let i = 0; i < 3; i++) {
+            await ctx.stub.deleteState(`${id}_segment${i}`);
+        }
+
+        // Delete checksum artifact
+        await ctx.stub.deleteState(`${id}_checksum`);
+
+        return `Artifact ${id} has been deleted successfully`;
     }
 
-    // TransferAsset updates the owner field of asset with given id in the world state.
-    async TransferAsset(ctx, id, newOwner) {
-        const assetString = await this.ReadAsset(ctx, id);
-        const asset = JSON.parse(assetString);
-        const oldOwner = asset.Owner;
-        asset.Owner = newOwner;
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
-        return oldOwner;
+    async ArtifactExists(ctx, id) {
+        const artifactJSON = await ctx.stub.getState(id);
+        return artifactJSON && artifactJSON.length > 0;
     }
 
-    // GetAllAssets returns all assets found in the world state.
-    async GetAllAssets(ctx) {
+    async TransferArtifact(ctx, id, newOwner) {
+        const artifactString = await this.ReadArtifact(ctx, id);
+        const artifact = JSON.parse(artifactString);
+        const oldIssuer = artifact.Issuer;
+        artifact.Issuer = newOwner;
+        await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(artifact))));
+        return oldIssuer;
+    }
+
+    async GetAllArtifacts(ctx) {
         const allResults = [];
-        // range query with empty string for startKey and endKey does an open-ended query of all assets in the chaincode namespace.
         const iterator = await ctx.stub.getStateByRange('', '');
         let result = await iterator.next();
         while (!result.done) {
@@ -164,4 +163,5 @@ class AssetTransfer extends Contract {
     }
 }
 
-module.exports = AssetTransfer;
+module.exports = ArtifactTransfer;
+
